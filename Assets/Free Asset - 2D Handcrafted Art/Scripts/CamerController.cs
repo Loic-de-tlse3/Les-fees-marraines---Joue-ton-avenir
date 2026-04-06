d7fa4290d782cfed46ff72ee3b933210f57a0bf5
Loic-de-tlse3 <loic.francois44@laposte.net>
2026-04-06 16:17:45 +0200

    Fusion projet de Lynda et develop

---
+++ Assets/Free Asset - 2D Handcrafted Art/Scripts/CamerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;  // ← nouveau namespace
+
+public class CamerController : MonoBehaviour {
+    public float speed;
+    public float clampLeft;
+    public float clampRight;
+    private float cameraX;
+
+    void Start () {
+        cameraX = transform.position.x;
+    }
+
+    void Update () {
+        if (Keyboard.current.rightArrowKey.isPressed && transform.position.x < clampRight)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Keyboard.current.leftArrowKey.isPressed && transform.position.x > clampLeft)
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Debug.Log(cameraX);
+        }
+    }
+}
